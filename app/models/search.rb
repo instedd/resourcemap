193e26f8a24d60a3fe42708d5dd2543a3e2c0e2d
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-03-06 18:14:36 -0300

    Support more search operations

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,10 +5,31 @@ class Search
 
   def eq(property, value)
     @search.filter :term, property => value
+    self
+  end
+
+  ['lt', 'lte', 'gt', 'gte'].each do |op|
+    class_eval %Q(
+      def #{op}(property, value)
+        @search.filter :range, property => {#{op}: value}
+      end
+    )
+  end
+
+  def op(op, property, value)
+    case op.to_s.downcase
+    when '<', 'l' then lt(property, value)
+    when '<=', 'lte' then lte(property, value)
+    when '>', 'gt' then gt(property, value)
+    when '>=', 'gte' then gte(property, value)
+    when '=', '==', 'eq' then eq(property, value)
+    else raise "Invalid operation: #{op}"
+    end
   end
 
   def where(properties = {})
     properties.each { |property, value| eq(property, value) }
+    self
   end
 
   def results
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -2,29 +2,70 @@ require 'spec_helper'
 
 describe Search do
   let!(:collection) { Collection.make }
-  let!(:site1) { collection.sites.make :properties => {'beds' => 5, 'tables' => 1} }
-  let!(:site2) { collection.sites.make :properties => {'beds' => 10, 'tables' => 2} }
-  let!(:site3) { collection.sites.make :properties => {'beds' => 20, 'tables' => 3} }
 
-  it "searches by equality" do
-    search = collection.new_search
-    search.where beds: 10
-    results = search.results
-    results.length.should eq(1)
-    results[0]['_id'].to_i.should eq(site2.id)
+  context "search by property" do
+    let!(:site1) { collection.sites.make :properties => {'beds' => 5, 'tables' => 1} }
+    let!(:site2) { collection.sites.make :properties => {'beds' => 10, 'tables' => 2} }
+    let!(:site3) { collection.sites.make :properties => {'beds' => 20, 'tables' => 3} }
+
+    it "searches by equality" do
+      search = collection.new_search
+      search.where beds: 10
+      assert_results search, site2
+    end
+
+    it "searches by equality of two properties" do
+      search = collection.new_search
+      search.where beds: 10, tables: 2
+      assert_results search, site2
+    end
+
+    it "searches by equality of two properties but doesn't find" do
+      search = collection.new_search
+      search.where beds: 10, tables: 1
+      search.results.length.should eq(0)
+    end
+
+    it "searches with lt" do
+      search = collection.new_search
+      search.lt :beds, 8
+      assert_results search, site1
+    end
+
+    it "searches with lte" do
+      search = collection.new_search
+      search.lte :beds, 10
+      assert_results search, site1, site2
+    end
+
+    it "searches with gt" do
+      search = collection.new_search
+      search.gt :beds, 18
+      assert_results search, site3
+    end
+
+    it "searches with gte" do
+      search = collection.new_search
+      search.gte :beds, 10
+      assert_results search, site2, site3
+    end
+
+    it "searches with combined properties" do
+      search = collection.new_search
+      search.lt :beds, 8
+      search.gte :tables, 1
+      assert_results search, site1
+    end
+
+    it "searches with ops" do
+      search = collection.new_search
+      search.op '<', :beds, 8
+      search.op '>=', :tables, 1
+      assert_results search, site1
+    end
   end
 
-  it "searches by equality of two properties" do
-    search = collection.new_search
-    search.where beds: 10, tables: 2
-    results = search.results
-    results.length.should eq(1)
-    results[0]['_id'].to_i.should eq(site2.id)
-  end
-
-  it "searches by equality of two properties but doesn't find" do
-    search = collection.new_search
-    search.where beds: 10, tables: 1
-    search.results.length.should eq(0)
+  def assert_results(search, *sites)
+    search.results.map{|r| r['_id'].to_i}.sort.should =~ sites.map(&:id)
   end
 end
