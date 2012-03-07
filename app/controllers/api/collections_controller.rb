f6e9a5ab95460fff12aaa569d653fd5458e61c81
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-03-07 19:59:37 -0300

    Filter rss results by updated since

--- app/controllers/api/collections_controller.rb
+++ app/controllers/api/collections_controller.rb
@@ -5,7 +5,8 @@ class Api::CollectionsController < ApplicationController
     search = collection.new_search
     search.page params[:page].to_i if params[:page]
     search.in_group params[:group] if params[:group]
-    search.where params.except(:action, :controller, :format, :id, :group, :page)
+    search.after params[:updated_since] if params[:updated_since]
+    search.where params.except(:action, :controller, :format, :id, :group, :page, :updated_since)
     @results = search.results
   end
 end
