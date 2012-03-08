c30bd53d701bc57734495d0f960a4c8342816e14
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-03-08 16:14:44 -0300

    Moved knockout helpers to a separate file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,7 @@
 module ApplicationHelper
+  include KnockoutHelper
+
   def google_maps_javascript_include_tag
     javascript_include_tag(raw("http://maps.googleapis.com/maps/api/js?sensor=false&key=#{GoogleMapsKey}"))
-  end
-
-  def ko_link_to(text, click, options = {})
-    link_to text, 'javascript:void()', options.merge(ko click: click)
-  end
-
-  def ko_link_to_root(text, click, options = {})
-    ko_link_to text, "$root.#{click}", options
-  end
-
-  def ko_text_field_tag(name, options = {})
-    text_field_tag name, '', ko(options.reverse_merge(value: name, valueUpdate: :afterkeydown))
-  end
-
-  def ko_number_field_tag(name, options = {})
-    number_field_tag name, '', ko(options.reverse_merge(value: name, valueUpdate: :afterkeydown))
-  end
-
-  def ko_check_box_tag(name, options = {})
-    check_box_tag name, '1', false, options.reverse_merge(ko checked: name)
-  end
-
-  def ko(hash = {})
-    {'data-bind' => kov(hash)}
-  end
-
-  def kov(hash = {})
-    hash.map do |k, v|
-      if v.respond_to? :to_hash
-        "#{k}:{#{kov(v)}}"
-      elsif v.to_s == 'valueUpdate'
-        "#{k}:'#{v}'"
-      else
-        "#{k}:#{v}"
-      end
-    end.join(',')
   end
 end
