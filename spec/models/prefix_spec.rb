66f268080698d92dde1ea64282cefaf7d6d66ee5
sreng <kengsreng@yahoo.com>
2012-05-21 17:34:51 +0700

    add prefix model spec test
    --HG--
    branch : migration

--- spec/models/prefix_spec.rb
+++ spec/models/prefix_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Prefix do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should get first next prefix" do
+    Prefix.next.version.should == "AA" 
+  end
+
+  it "should get next prefix" do
+    Prefix.create :version => 'AX'
+    Prefix.next.version.should == 'AY'
+  end
+  
+  it "should save prefix after get next prefix" do
+    lambda {
+      Prefix.next
+    }.should change { Prefix.count }.by(1)
+  end
 end
