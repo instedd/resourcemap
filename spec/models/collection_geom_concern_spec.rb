0d900b346b1c17ccd10c40a24c8a086b9b7f6460
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-05-25 13:26:52 +0700

    Set collection center to be the center of the bounding box
    --HG--
    branch : plain_coffee

--- spec/models/collection_geom_concern_spec.rb
+++ spec/models/collection_geom_concern_spec.rb
@@ -12,6 +12,16 @@ describe Collection::GeomConcern do
     collection.lng.to_f.should eq(25.0)
   end
 
+  it "calculates center from children sites is not weighted" do
+    site1 = collection.sites.make :lat => 30, :lng => 20
+    site2 = collection.sites.make :lat => 40, :lng => 30
+    site3 = collection.sites.make :lat => 40, :lng => 30
+
+    collection.reload
+    collection.lat.to_f.should eq(35.0)
+    collection.lng.to_f.should eq(25.0)
+  end
+
   it "ignores sites without lat/lng", :focus => true do
     site1 = collection.sites.make :lat => nil, :lng => nil
     site2 = collection.sites.make :lat => 30, :lng => 20
