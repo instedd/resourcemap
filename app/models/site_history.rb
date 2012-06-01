a35f1954427183b0fb6fe01c6bef9ccf65f5325d
Carolina Hadad <chadad@manas.com.ar>
2012-06-01 11:33:24 +0700

    Site History stored when saving site
    --HG--
    branch : plain_coffee

--- app/models/site_history.rb
+++ app/models/site_history.rb
@@ -1,3 +1,8 @@
 class SiteHistory < ActiveRecord::Base
   belongs_to :site
+
+  def self.create_from_site(site)
+    SiteHistory.create(:collection_id => site.collection_id, :name => site.name, :lat =>site.lat, :lng => site.lng, :parent_id => site.parent_id, :hierarchy => site.hierarchy, :properties => site.properties, :location_mode => site.location_mode, :id_with_prefix => site.id_with_prefix, :valid_since => Time.now, :site_id => site.id)
+
+  end
 end
--- spec/models/site_history_spec.rb
+++ spec/models/site_history_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe SiteHistory do
   it { should belong_to :site }
+
+  it "should create from site" do
+
+    site = Site.make
+    site_history = SiteHistory.create_from_site site
+    site_history.site_id.should == site.id
+    site_history.collection_id.should == site.collection_id
+    site_history.name.should == site.name
+    site_history.lat.should == site.lat
+    site_history.lng.should == site.lng
+    site_history.parent_id.should == site.parent_id
+    site_history.hierarchy.should == site.hierarchy
+    site_history.properties.should == site.properties
+    site_history.location_mode.should == site.location_mode
+    site_history.id_with_prefix.should == site.id_with_prefix
+    site_history.valid_to.should == nil
+    site_history.valid_since.to_i.should eq(Time.now.to_i)
+
+  end
+
+
 end
