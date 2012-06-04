47d328afa4941e1656a329bc9bfc45b6745e9c11
Carolina Hadad <chadad@manas.com.ar>
2012-06-04 12:01:53 +0700

    Refactor in VersioningConcern in progress
    --HG--
    branch : plain_coffee
    extra : rebase_source : 398e3ec5bee43ecd919ed1a5044ade8fe1ea2777

--- app/models/versioning_concern.rb
+++ app/models/versioning_concern.rb
@@ -1,15 +1,24 @@
 module VersioningConcern
+  extend ActiveSupport::Concern
 
-  def create_history(history_class, object)
-    history = history_class.get_current_value object
-    if history
-      history.set_valid_to object.updated_at
-    end
-    history_class.create_from_site object
+  included do
+    after_save :create_history
+    before_destroy :set_history_expiration
   end
 
-  def set_history_expiration(history_class, object)
-    history = history_class.get_current_value object
+  def create_history
+    history_class = "#{self.class.name}History".constantize
+    history = history_class.get_current_value self
+    if history
+      history.set_valid_to self.updated_at
+    end
+    history_class.create_from_site self
+    self.site_histories.insert(history)
+  end
+
+  def set_history_expiration
+    history_class = "#{self.class.name}History".constantize
+    history = history_class.get_current_value self
     history.set_valid_to Time.now
   end
 end
