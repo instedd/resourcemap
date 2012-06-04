a78ccd8ca3c2b2cb2f1035a48993fa310a306f86
sreng <kengsreng@yahoo.com>
2012-06-04 14:38:39 +0700

    add exec_visitor_test and fix some issue on sms query and update
    --HG--
    branch : plain_coffee

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -13,7 +13,7 @@ class Collection < ActiveRecord::Base
   has_many :fields, order: 'ord'
   has_many :thresholds
   
-  OPERATOR = {">" => "gt", "<" => "lt", ">=" => "gte", "<=" => "lte", "=>" => "gte", "=<" => "lte"}
+  OPERATOR = {">" => "gt", "<" => "lt", ">=" => "gte", "<=" => "lte", "=>" => "gte", "=<" => "lte", "=" => "eq"}
 
   def max_value_of_property(es_code)
     search = new_tire_search
@@ -85,9 +85,9 @@ class Collection < ActiveRecord::Base
   def query_sites(option)
     operator = operator_parser option[:operator]
     field = Field.find_by_code option[:code]
-    
     search = self.new_search 
     search.use_codes_instead_of_es_codes
+     
     search.send operator , option[:code], option[:value]
     results = search.results
     response_prepare(option[:code], field.id, results) 
