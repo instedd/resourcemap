2c7c9f13d943804ca6f22dceb88c61866fa96840
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-06-04 17:39:15 +0800

    Fixed history migration to use current code
    --HG--
    branch : plain_coffee

--- db/migrate/20120604024623_add_history_to_existing_sites.rb
+++ db/migrate/20120604024623_add_history_to_existing_sites.rb
@@ -1,11 +1,7 @@
 class AddHistoryToExistingSites < ActiveRecord::Migration
   def up
     Site.find_each do |site|
-      sh = SiteHistory.find_by_site_id site.id
-      if(!sh)
-        shist = SiteHistory.create_from_site site
-        shist.save
-      end
+      site.create_history unless site.current_history
     end
   end
 
