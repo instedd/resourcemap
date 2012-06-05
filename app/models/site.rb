89c479a3137ad614c0b1fa1e632fa757fe8fbe76
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-06-05 18:36:23 +0800

    Moved site properties' cleanup funcionality into a concern
    --HG--
    branch : plain_coffee

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,7 @@
 class Site < ActiveRecord::Base
   include Activity::AwareConcern
   include Site::ActivityConcern
+  include Site::CleanupConcern
   include Site::GeomConcern
   include Site::PrefixConcern
   include Site::TireConcern
@@ -9,20 +10,6 @@ class Site < ActiveRecord::Base
   belongs_to :collection
 
   serialize :properties, Hash
-  before_save :strongly_type_properties
-
-  def strongly_type_properties
-    fields = collection.fields.index_by(&:es_code)
-    self.properties.keys.each do |key|
-      field = fields[key]
-      self.properties[key] = field.strongly_type(properties[key]) if field
-    end
-  end
-
-  before_save :remove_nil_properties, :if => :properties
-  def remove_nil_properties
-    self.properties.reject! { |k, v| v.nil? }
-  end
 
   def update_properties(site, user, props)
     props.each do |p|
