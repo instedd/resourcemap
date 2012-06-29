350940f075cb3484a6f3b74a1cfa118054393263
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-06-29 15:54:15 -0300

    Reuse configuration files on deploy
    --HG--
    branch : plain_coffee

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,14 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  task :symlink_configs, :roles => :app do
+    %W(settings.yml google_maps.key nuntium.yml).each do |file|
+      run "ln -nfs #{shared_path}/#{file} #{release_path}/config/"
+    end
+  end
 end
 
 before "deploy:start", "deploy:migrate"
 before "deploy:restart", "deploy:migrate"
+after "deploy:update_code", "deploy:symlink_configs"
