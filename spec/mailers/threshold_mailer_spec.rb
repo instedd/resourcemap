25162e5a17914e5268d0ddab7c2e9c54ee30d585
DaneSavot <dane@instedd.org>
2012-07-03 22:57:20 +0700

    Add spec to threshold_mailer
    --HG--
    branch : plain_coffee

--- spec/mailers/threshold_mailer_spec.rb
+++ spec/mailers/threshold_mailer_spec.rb
@@ -1,5 +1,20 @@
 require "spec_helper"
 
 describe ThresholdMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:users) { [User.make, User.make]}
+  let!(:message) {"testing"}
+  let!(:mail) { ThresholdMailer.notify_email(message, users.map(&:id)) }
+  it "has email in queue" do 
+    mail.deliver
+    ActionMailer::Base.deliveries.empty?.should_not be_true
+  end 
+  it "send to correct email" do
+    mail.deliver
+    mail.to.should eq(users.map(&:email))
+  end
+  it "send a correct email" do
+    mail.deliver
+    mail.body.should eq(message)
+  end
+
 end
