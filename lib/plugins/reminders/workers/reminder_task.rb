616ab8ea32661d004ffac23108fea50c6fb8f0d9
sreng <kengsreng@yahoo.com>
2012-07-18 17:54:49 +0700

    load email and phone depend on sites under reminder
    --HG--
    branch : plugins

--- lib/plugins/reminders/workers/reminder_task.rb
+++ lib/plugins/reminders/workers/reminder_task.rb
@@ -1,9 +1,16 @@
 class ReminderTask
   def self.perform    
     Reminder.where("next_run <= ?", Time.now).each do |reminder|
-      puts reminder.reminder_message  
       reminder.save! 
-      # adding reminder into que
+      sites = reminder.sites
+      sites.each do |site|
+        email = site.properties[site.collection.fields.select { |f| f.kind =='email'}.first.id.to_s]
+        phone = site.properties[site.collection.fields.select { |f| f.kind =='phone'}.first.id.to_s]
+        reminder_message = sites.reminder_message  
+        # send sms and email with reminder_message   
+        #
+      end
+      reminder.save! 
     end
   end
 end
