9a858bf3f8c4ea2c3513a0df4c1b9573e7bdb3d9
sreng <kengsreng@yahoo.com>
2012-07-24 09:20:39 +0700

    Added foreman tasks to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,32 @@ namespace :deploy do
   end
 end
 
+namespace :foreman do
+  desc 'Export the Procfile to Ubuntu upstart scripts'
+  task :export, :roles => :app do
+    run "echo -e \"PATH=$PATH\\nGEM_HOME=$GEM_HOME\\nGEM_PATH=$GEM_PATH\\nRAILS_ENV=production\" >  #{current_path}/.env"
+    run "cd #{current_path} && rvmsudo bundle exec foreman export upstart /etc/init -f #{current_path}/Procfile -a #{application} -u #{user} --concurrency=\"resque=1,resque_scheduler=1\""
+  end
+
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start #{application}"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop #{application}"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    run "sudo start #{application} || sudo restart #{application}"
+  end
+end
+
 before "deploy:start", "deploy:migrate"
 before "deploy:restart", "deploy:migrate"
 after "deploy:update_code", "deploy:symlink_configs"
+
+after "deploy:update", "foreman:export"    # Export foreman scripts
+after "deploy:restart", "foreman:restart"   # Restart application scripts
