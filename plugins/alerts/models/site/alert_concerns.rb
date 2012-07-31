26e6b39bbc2276ed9ea8d3e4e6bda95e7f742bff
sreng <kengsreng@yahoo.com>
2012-07-31 15:19:04 +0700

    added rspec for alert_concerns.rb and fixed some issues

--- plugins/alerts/models/site/alert_concerns.rb
+++ plugins/alerts/models/site/alert_concerns.rb
@@ -13,19 +13,17 @@ module Site::AlertConcerns
       if alert.is_notify
         users_phone_number = []
         users_email = []
-        users_phone_number << User.find(alert.phone_notification[:members]).map { |user| user.phone_number}
-        users_email << User.find(alert.email_notification[:members]).map { |user| user.email}
+        users_phone_number << User.find(alert.phone_notification[:members]).map { |user| user.phone_number} if alert.phone_notification[:members]
+        users_email << User.find(alert.email_notification[:members]).map { |user| user.email} if alert.email_notification[:members]
+        
         alert.email_notification.except(:members).values.flatten.each do |field|
           users_email << properties[field] 
         end
+                
+        alert.phone_notification[:fields].each {|field| users_phone_number << properties[field]}  if alert.phone_notification[:fields]
         
-        alert.phone_notification[:fields].each do |field|
-          users_phone_number << properties[field] 
-        end
+        alert.phone_notification[:users].each {|user| users_phone_number << User.find_all_by_email(properties[user]).map { |user| user.phone_number} if properties[user]} if alert.phone_notification[:users]
         
-        alert.phone_notification[:users].each do |user|
-          users_phone_number << User.find_all_by_email(properties[user]).map { |user| user.phone_number} # get users phone_number based on users_email
-        end
         message_notification = alert.message_notification.render_template_string(get_template_value_hash)
         Resque.enqueue SmsTask, users_phone_number.flatten.compact, message_notification unless users_phone_number.empty?
         Resque.enqueue EmailTask, users_email.flatten.compact, message_notification, "[ResourceMap] Alert Notification" unless users_email.empty?
--- plugins/alerts/spec/models/site_alert_concerns_spec.rb
+++ plugins/alerts/spec/models/site_alert_concerns_spec.rb
@@ -7,37 +7,127 @@ describe Site::AlertConcerns do
   let!(:phone_field) { layer.fields.make :code => 'phone' } 
   let!(:email_field) { layer.fields.make :code => 'email' } 
   let!(:user_field) { layer.fields.make :code => 'user' } 
+  let!(:users) { [User.make(:email => 'user@instedd.org', :password => '1234567', :phone_number => '855123456789'), User.make(:email => 'demo@instedd.org', :password => '1234567', :phone_number => '855123333444')]}
+  let!(:site1) { collection.sites.make :properties => { bed_field.es_code => 15, phone_field.es_code => users[0].phone_number, email_field.es_code => users[0].email, user_field.es_code => users[0].email}} 
   
-  describe "adding queue when hit alert threshold" do 
-    let!(:users) { [User.make(:email => 'user@instedd.org', :password => '1234567', :phone_number => '855123456789')]}
-    let!(:site1) { collection.sites.make :properties => {bed_field.es_code => 15, phone_field.es_code => users[0].phone_number, email_field.es_code => users[0].email, user_field.es_code => users[0].email}}
-    let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {users: [user_field.es_code], fields: [email_field.es_code], members: [users[0].id]}, phone_notification: {users: [user_field.es_code], fields: [phone_field.es_code], members: [users[0].id]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
-    let!(:message_notification) { "alert sms"} 
-     
-    describe "add new site" do
-      let!(:site) {collection.sites.make :properties => {bed_field.es_code => 5, phone_field.es_code => users[0].phone_number, email_field.es_code => users[0].email, user_field.es_code => users[0].email}}
-      it "should add sms_que into Resque.enqueue with users and message_notification" do 
-        SmsTask.should have_queued([users[0].phone_number, users[0].phone_number, users[0].phone_number], message_notification).in(:sms_queue)
+  describe "add new site" do 
+    describe "when hit threshold" do
+      describe "send email and sms to all selected users_field" do 
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {users: [user_field.es_code]}, phone_notification: {users: [user_field.es_code]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :gt, value: 10 ]}
+        let!(:site) {collection.sites.make :properties => {bed_field.es_code => 15, user_field.es_code => users[0].email}}
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[0].phone_number], threshold.message_notification).in(:sms_queue)
+        end
+
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[0].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
+      end 
+
+      describe "send email and sms to all selected all members" do
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {members: [users[0].id, users[1].id]}, phone_notification: {members: [users[0].id, users[1].id]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
+        let!(:site) {collection.sites.make :properties => {bed_field.es_code => 5}}
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[0].phone_number, users[1].phone_number], threshold.message_notification).in(:sms_queue)
+        end
+
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[0].email, users[1].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
       end
 
-      it "should add email_que into Resque.enqueue with threshold and message_notification" do 
-        EmailTask.should have_queued([users[0].email, users[0].email, users[0].email], message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
-      end
-    end
+      describe "send email and sms to all selected fields" do
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {fields: [email_field.es_code]}, phone_notification: {fields: [phone_field.es_code]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
+        let!(:site) {collection.sites.make :properties => {bed_field.es_code => 5, phone_field.es_code => users[1].phone_number, email_field.es_code => users[1].email}}
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[1].phone_number], threshold.message_notification).in(:sms_queue)
+        end
 
-    describe "edit site" do
-      it "should add sms_que into Resque.enqueue with users and message_notification" do 
-        site1.properties = {bed_field.es_code => 5, phone_field.es_code => [], email_field.es_code => users[0].email, user_field.es_code => users[0].email}
-        site1.save 
-        SmsTask.should have_queued([users[0].phone_number, users[0].phone_number], message_notification).in(:sms_queue)
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[1].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
       end
+      
+      describe "send email and sms to all selected fields, members and users" do
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {members: [users[0].id], fields: [email_field.es_code], users: [user_field.es_code]}, phone_notification: { members: [users[1].id], fields: [phone_field.es_code], users: [user_field.es_code]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
+        let!(:site) {collection.sites.make :properties => {bed_field.es_code => 5, phone_field.es_code => users[1].phone_number, email_field.es_code => users[0].email, user_field.es_code => users[0].email}}
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[1].phone_number, users[1].phone_number, users[0].phone_number], threshold.message_notification).in(:sms_queue)
+        end
 
-      it "should add email_que into Resque.enqueue with threshold and message_notification" do 
-        site1.properties = {bed_field.es_code => 5, user_field.es_code => users[0].email}
-        site1.save 
-        EmailTask.should have_queued([users[0].email, users[0].email], message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[0].email, users[0].email, users[0].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
       end
     end
   end
+  
+  describe "edit site" do
+    describe "when hit threshold" do
+      describe "send email and sms to all selected users_field" do 
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {users: [user_field.es_code]}, phone_notification: {users: [user_field.es_code]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
+        before(:each) do
+          ResqueSpec.reset!
+          site1.properties = {bed_field.es_code => 5, user_field.es_code => users[0].email}
+          site1.save
+        end
+        
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[0].phone_number], threshold.message_notification).in(:sms_queue)
+        end
 
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[0].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
+      end 
+
+      describe "send email and sms to all selected members" do
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {members: [users[0].id, users[1].id]}, phone_notification: {members: [users[0].id, users[1].id]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 20 ]}
+        before(:each) do
+          ResqueSpec.reset!
+          site1.properties = {bed_field.es_code => 15}
+          site1.save 
+        end
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[0].phone_number, users[1].phone_number], threshold.message_notification).in(:sms_queue)
+        end
+
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[0].email, users[1].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
+      end
+
+      describe "send email and sms to all selected fields" do
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {fields: [email_field.es_code]}, phone_notification: {fields: [phone_field.es_code]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
+        before(:each) do
+          ResqueSpec.reset!
+          site1.properties = {bed_field.es_code => 5, phone_field.es_code => users[1].phone_number, email_field.es_code => users[1].email}
+          site1.save 
+        end
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[1].phone_number], threshold.message_notification).in(:sms_queue)
+        end
+
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[1].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
+      end
+      
+      describe "send email and sms to all selected fields, members and users" do
+        let!(:threshold){ collection.thresholds.make is_notify: true, is_all_site: true, email_notification: {members: [users[0].id], fields: [email_field.es_code], users: [user_field.es_code]}, phone_notification: { members: [users[1].id], fields: [phone_field.es_code], users: [user_field.es_code]}, message_notification: "alert sms", conditions: [ field: bed_field.es_code, op: :lt, value: 10 ]}
+        before(:each) do
+          ResqueSpec.reset!
+          site1.properties = {bed_field.es_code => 5, phone_field.es_code => users[1].phone_number, email_field.es_code => users[0].email, user_field.es_code => users[0].email}
+          site1.save
+        end
+        it "should add sms_que into Resque.enqueue" do 
+          SmsTask.should have_queued([users[1].phone_number, users[1].phone_number, users[0].phone_number], threshold.message_notification).in(:sms_queue)
+        end
+
+        it "should add email_que into Resque.enqueue" do 
+          EmailTask.should have_queued([users[0].email, users[0].email, users[0].email], threshold.message_notification, "[ResourceMap] Alert Notification").in(:email_queue)
+        end
+      end
+    end
+  end
 end
