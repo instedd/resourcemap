82eaeaa1fa9e2a12ee01b9867145b4e714b225d9
mann <chanmann@instedd.org>
2012-08-08 11:27:12 +0700

    add more repeats in db seed
    --HG--
    extra : rebase_source : e0df71d7f5bb1c17e1254912e82f13bd238f8f3c

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Repeat.find_or_create_by_name(name: "from Monday to Friday", order: 1, rule: IceCube::Rule.weekly.day(:monday, :tuesday, :wednesday, :thursday, :friday))
+Repeat.find_or_create_by_name(name: "Everyday", order: 2, rule: IceCube::Rule.daily)
+Repeat.find_or_create_by_name(name: "Every week", order: 3, rule: IceCube::Rule.weekly)
+Repeat.find_or_create_by_name(name: "Every 2 weeks", order: 4, rule: IceCube::Rule.weekly(2))
+Repeat.find_or_create_by_name(name: "Every month", order: 5, rule: IceCube::Rule.monthly)
