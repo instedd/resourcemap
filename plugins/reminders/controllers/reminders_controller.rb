173b08d43278c2f8e52009e21a368037e9302910
sreng <kengsreng@yahoo.com>
2012-08-14 16:42:03 +0700

    reminder: fixed send reminder_message to multi sites

--- plugins/reminders/controllers/reminders_controller.rb
+++ plugins/reminders/controllers/reminders_controller.rb
@@ -21,7 +21,7 @@ class RemindersController < ApplicationController
   def update
     reminder = reminders.find params[:id]
     reminder.update_attributes! params[:reminder].except(:sites)
-    reminder.sites = Site.select("id, name, properties").find params[:reminder][:sites] if params[:reminder][:sites]
+    reminder.sites = Site.select("id, collection_id, name, properties").find params[:reminder][:sites] if params[:reminder][:sites]
     
     reminder.save! 
     render json: reminder
--- plugins/reminders/workers/reminder_task.rb
+++ plugins/reminders/workers/reminder_task.rb
@@ -6,12 +6,16 @@ class ReminderTask
       users_email = []
       users_phone_number = []
       reminder.target_sites.each do |site|
-        users_email << site.properties[site.collection.fields.select { |f| f.kind =='email'}.first.id.to_s]
-        users_phone_number << site.properties[site.collection.fields.select { |f| f.kind =='phone'}.first.id.to_s]
+        users_email << ReminderTask.get_site_properties_value_by_kind(site, 'email')
+        users_phone_number << ReminderTask.get_site_properties_value_by_kind(site, 'phone')
       end
       message_reminder = reminder.reminder_message
-      Resque.enqueue SmsTask, users_phone_number.compact, message_reminder unless users_phone_number.empty?
-      Resque.enqueue EmailTask, users_email.compact, message_reminder, "[ResourceMap] Reminder Notification"  unless users_email.empty?
+      Resque.enqueue SmsTask, users_phone_number.flatten, message_reminder unless users_phone_number.empty?
+      Resque.enqueue EmailTask, users_email.flatten, message_reminder, "[ResourceMap] Reminder Notification"  unless users_email.empty?
     end
+  end
+
+  def self.get_site_properties_value_by_kind(site, kind)
+    site.collection.fields.select { |f| f.kind == kind }.map{ |field| site.properties[field.id.to_s] }
   end
 end
