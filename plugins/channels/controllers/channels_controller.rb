c99f2138d230d69c21da90e6e49b1d9ac90d2201
sreng <kengsreng@yahoo.com>
2012-08-22 15:56:57 +0700

    channel: generate channel plugin file and folder structure

---
+++ plugins/channels/controllers/channels_controller.rb
@@ -0,0 +1,4 @@
+class ChannelsController < ApplicationController
+  before_filter :authenticate_user!
+
+end
---
+++ plugins/channels/plugin.rb
@@ -0,0 +1,39 @@
+class Channels::Plugin < Plugin
+  # Sample collection tab
+  #   collection_tab "/channels_tab"
+
+  # Sample map header view injection
+  #   map_header "/alerts_map_header"
+
+  # Sample site clusterer extension
+  #   clusterer \
+  #     map: ->(site, hash) do
+  #       hash[:alert_count] ||= 0
+  #       hash[:alert_count] += (site[:alert] == 'true' ? 1 : 0)
+  #     end,
+  #     reduce: ->(hash, cluster) { cluster[:alert_count] = hash[:alert_count] }
+
+  # Sample schedule task
+  #   schedule \
+  #     every: "1m",
+  #     class: "ChannelsTask",
+  #     queue: "channels_queue"
+
+  # Sample model extension
+  #   extend_model \
+  #     class: Site,
+  #     with: Site::ChannelsConcerns
+
+  # Sample field type extension
+  #   field_type \
+  #     name: "user",
+  #     css_class: "luser",
+  #     edit_view: "fields/user_edit_view"
+
+  # Sample routes
+  #   routes {
+  #     resources :collections do
+  #       resources :channels
+  #     end
+  #   }
+end
