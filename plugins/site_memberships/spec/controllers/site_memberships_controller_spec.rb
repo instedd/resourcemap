df511e8cc06f3cb8ae7f90dee785e71dddb3d25e
mann <chanmann@instedd.org>
2012-09-06 18:48:28 +0700

    apply collection admin authentication for setting site memberships assess
    --HG--
    extra : rebase_source : 5f8484b2c2664c2ca75b95ba4dc4021cb30f17cf

--- plugins/site_memberships/spec/controllers/site_memberships_controller_spec.rb
+++ plugins/site_memberships/spec/controllers/site_memberships_controller_spec.rb
@@ -20,5 +20,22 @@ describe SiteMembershipsController do
       post :set_access, access: false, type: 'update_access', field_id: field.id, collection_id: collection.id 
       collection.site_memberships.first.update_access.should be false
     end
+
+    context "when user is not collection admin" do
+      let!(:membership) { collection.memberships.first }
+
+      before(:each) do
+        membership.update_attributes admin: false
+        post :set_access, access: true, type: 'view_access', field_id: field.id, collection_id: collection.id
+      end
+
+      it "should not set view access" do
+        collection.site_memberships.count.should == 0
+      end
+
+      it "should respond unauthorized header" do
+        response.status.should == 401
+      end
+    end
   end
 end
