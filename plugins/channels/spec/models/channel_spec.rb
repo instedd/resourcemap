0b68d2e1ea1ab889053a05a4899ef0da3a00fa41
sreng <kengsreng@yahoo.com>
2012-09-07 10:15:41 +0700

    channel: added channel_model spec_test

--- plugins/channels/spec/models/channel_spec.rb
+++ plugins/channels/spec/models/channel_spec.rb
@@ -1,3 +1,49 @@
 require 'spec_helper'
 describe Channel do
+  it { should validate_presence_of :name }
+
+  it 'should valid when name is more than 4 charracters' do
+      channel = Channel.new name: 'abcd', password: '12345', is_manual_configuration: true
+      channel.valid?.should be_true
+  end
+
+  it 'should not valid when name is less than 4 characters' do 
+    channel = Channel.new name: "abc", password: "12345"
+    channel.should_not be_valid
+  end
+
+  describe 'when is is_manual_configuration == true' do
+    before(:each) do
+      @channel = Channel.new name: 'abcd', password: '', is_manual_configuration: true
+    end
+    
+    it 'should require password' do
+      @channel.valid?.should be_false
+    end
+
+    it 'should require at least 4 character of password' do
+      @channel.password = '123'
+      @channel.valid?.should be_false
+    end
+    
+    it 'should be valid when password presence and more than 4 characters' do
+      @channel.password = '12345'
+      @channel.valid?.should be_true
+    end
+  end
+  
+  describe 'when is_manual_configuration == false' do
+    before(:each) do
+      @channel = Channel.new name: 'abcd', ticket_code: '', is_manual_configuration: false
+    end
+
+    it 'should require ticket_code' do
+      @channel.valid?.should be_false
+    end
+
+    it 'should valid when ticket_code presence' do
+      @channel.ticket_code = '1234'
+      @channel.valid?.should be_true
+    end
+  end
 end
