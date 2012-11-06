0adbe0c3956e3ead91c2ce7c3c773a63279b1efd
FaQ <fciccioli@manas.com.ar>
2012-11-06 17:24:55 -0300

    Fixed issue with spork not reloading the classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,8 @@ Spork.prefork do
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+  Spork.trap_class_method ActionDispatch::Reloader, :to_prepare if ENV["DRB"]
   require File.expand_path("../../config/environment", __FILE__)
-  require File.expand_path("../../spec/blueprints", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
 
@@ -64,7 +64,7 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-
+  require File.expand_path("../../spec/blueprints", __FILE__)
 end
 
 
