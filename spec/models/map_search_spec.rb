73e6b472aa2c0cc55c4ba406a442c2e02dd7a228
Carolina Hadad <chadad@manas.com.ar>
2013-01-10 12:31:44 -0300

    test fixed! all tests are passing :)

--- spec/models/map_search_spec.rb
+++ spec/models/map_search_spec.rb
@@ -22,7 +22,7 @@ describe MapSearch do
     lat: site.lat.to_f, lng: site.lng.to_f,
     name: site.name, name_not_analyzed: site.name, id_with_prefix:"AA1",
     :lat_analyzed=> site.lat.to_s, :lng_analyzed=> site.lng.to_s,
-    highlighted: false, icon: 'null'}
+    highlighted: false, icon: 'null', alert: 'false'}
     search.results[:sites][0].should == expected_hash
   end
 
