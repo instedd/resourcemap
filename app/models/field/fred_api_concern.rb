4bd3a264c49886d989b5e10f64a1a639c76dc5a6
Carolina Hadad <chadad@manas.com.ar>
2013-02-08 14:16:39 -0300

    field's metadata keys are case insensitive

--- app/models/field/fred_api_concern.rb
+++ app/models/field/fred_api_concern.rb
@@ -4,15 +4,15 @@ module Field::FredApiConcern
   # metadata: {"0"=>{"key"=>"context", "value"=>"MOH"}, "1"=>{"key"=>"agency", "value"=>"DHIS"}}
 
   def identifier?
-    metadata && metadata.any?{|index, element| element["key"] == "agency"} && metadata.any?{|index, element| element["key"] == "context"}
+    metadata && metadata.any?{|index, element| element["key"].downcase == "agency"} && metadata.any?{|index, element| element["key"].downcase == "context"}
   end
 
   def context
-    metadata.select{|i, e| e["key"] == "context"}.values[0]["value"]
+    metadata.select{|i, e| e["key"].downcase == "context"}.values[0]["value"]
   end
 
   def agency
-    metadata.select{|i, e| e["key"] == "agency"}.values[0]["value"]
+    metadata.select{|i, e| e["key"].downcase == "agency"}.values[0]["value"]
   end
 
 end
--- spec/controllers/fred_api/fred_api_controller_spec.rb
+++ spec/controllers/fred_api/fred_api_controller_spec.rb
@@ -319,6 +319,17 @@ describe FredApi::FredApiController do
       json["identifiers"][0].should eq({"context" => "MOH", "agency" => "DHIS", "id"=> "53adf"})
     end
 
+    it "creation of identifiers field should be case insensitive" do
+      id_case_insensitive = layer.fields.make :code => 'moh-id-case-insensitive', :kind => 'text', :metadata =>  {"0"=>{"key"=>"Context", "value"=>"MOH2"}, "1"=>{"key"=>"AGency", "value"=>"DHIS2"} }
+      site_with_2_ids = collection.sites.make :properties => {
+          moh_id.es_code => "53adf",
+          date.es_code => "2012-10-24T00:00:00Z",
+          id_case_insensitive.es_code => "23415"
+        }
+      get :show_facility, id: site_with_2_ids.id, format: 'json', collection_id: collection.id
+      json = JSON.parse response.body
+      json["identifiers"].length.should eq(2)
+    end
   end
 
 end
