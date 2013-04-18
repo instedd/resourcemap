de477a15d34ea6d93500dc56aa9a7d4661c593b8
Mauri <mmuller@manas.com.ar>
2013-04-18 15:33:33 -0300

    Added Capybara settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/integration/spec/helpers/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -19,13 +20,48 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  ###########
+  #capybara
+  config.include Capybara::DSL,           example_group: { file_path: config.escaped_path(%w[spec integration])}
+  config.include Capybara::CustomFinders, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  #config.include Capybara::AccountHelper, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  #config.include Capybara::CollectionsHelper, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  #config.include Capybara::SettingsHelper, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  #config.include Capybara::MailHelper, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  config.filter_run_excluding(js: true)   unless config.filter_manager.inclusions[:js]
+
+  
+  Capybara.default_wait_time = 5
+  Capybara.javascript_driver = :selenium
+  Capybara.default_selector = :css
+
+  config.before :each do
+    DatabaseCleaner.strategy = if Capybara.current_driver == :rack_test
+      :transaction
+    else
+      [:truncation]
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+  ##########
+
+  # Uncomment to view full backtraces
+  # config.backtrace_clean_patterns = []
+
+  # Helper for ignoring blocks of tests
+  def ignore(*args); end;
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
