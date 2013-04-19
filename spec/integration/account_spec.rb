b1b8a9dadc0d0c0540309fe3505a01f6e50dc8d9
Mauri <mmuller@manas.com.ar>
2013-04-19 13:08:43 -0300

    Isolated failed Acceptance Tests

--- spec/integration/account_spec.rb
+++ spec/integration/account_spec.rb
@@ -4,7 +4,8 @@ describe "Accounts" do
 
   it "should create an account", js:true do
    @user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
-   login_as (@user)   
+   login_as (@user)
+   visit collections_path   
   end
 
 end
