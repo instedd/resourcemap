caac0c787e0d51fe7970c801585604adec800166
German Batiston <gbatiston@manas.com.ar>
2013-04-22 10:44:40 -0300

    Gemfile lock

--- spec/integration/account_spec.rb
+++ spec/integration/account_spec.rb
@@ -5,7 +5,7 @@ describe "Accounts" do
   it "should create an account", js:true do
    @user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
    login_as (@user)
-   visit collections_path   
+   visit collections_path
   end
 
 end
