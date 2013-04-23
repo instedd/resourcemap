5e50d2f6abc3bbea77a169fac54966bc69090e52
Mauri <mmuller@manas.com.ar>
2013-04-23 12:29:58 -0300

    Automated field creation. Added Edit Field test

--- spec/integration/collections/edit_field_spec.rb
+++ spec/integration/collections/edit_field_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper' 
 
-describe "edit_layer" do 
+describe "edit_field" do 
  
-  it "edit layer", js:true do
+  it "should edit field", js:true do
     @user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
     page.save_screenshot 'edit_layer.png'
     collection = create_collection_for(@user)
     @layer = create_layer_for
+    field = create_field_for
     login_as (@user)
     visit collections_path
+    sleep 30
   end
 end
--- spec/support/capybara/collection_helper.rb
+++ spec/support/capybara/collection_helper.rb
@@ -8,12 +8,13 @@ module Capybara::CollectionHelper
 
   def create_site_for(collection)
   	site = Site.make(:collection => collection)
-  	#site = Site.make(:name => 'Health Center')
-    #@user.create_site(site)
-    #@user.memberships.first.should be_admin
   end
 
   def create_layer_for
-  	Layer.make(collection_id: 1, :name => 'This Layer belongs to the Central Hospital')
+  	Layer.make(collection_id: 1, :name => 'Central Hospital Layer 1')
+  end
+
+  def create_field_for
+  	field = Field::TextField.make(:name => 'Central Hospital Layer 1 Field', collection_id: 1, layer_id: 1)
   end
 end
