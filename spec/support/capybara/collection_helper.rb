38fbae04a4e997d2974df62237c3756155fd9cd7
Mauri <mmuller@manas.com.ar>
2013-04-23 10:05:31 -0300

    Added Edit Layer test

--- spec/support/capybara/collection_helper.rb
+++ spec/support/capybara/collection_helper.rb
@@ -5,4 +5,15 @@ module Capybara::CollectionHelper
     @user.create_collection(collection)
     @user.memberships.first.should be_admin
   end
+
+  def create_site_for(collection)
+  	site = Site.make(:collection => collection)
+  	#site = Site.make(:name => 'Health Center')
+    #@user.create_site(site)
+    #@user.memberships.first.should be_admin
+  end
+
+  def create_layer_for
+  	Layer.make(collection_id: 1, :name => 'This Layer belongs to the Central Hospital')
+  end
 end
