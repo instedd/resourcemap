34d0f175e316e7f5ccb382cc05df3075bef84b24
Mauri <mmuller@manas.com.ar>
2013-04-23 14:02:22 -0300

    Added Edit Site test

--- spec/support/capybara/collection_helper.rb
+++ spec/support/capybara/collection_helper.rb
@@ -1,13 +1,13 @@
 module Capybara::CollectionHelper
 
-  def create_collection_for(user)
-  	collection = Collection.make(:name => 'Central Hospital')
-    @user.create_collection(collection)
-    @user.memberships.first.should be_admin
+  def create_collection_for (user)
+    collection = user.collections.make name: 'Central Hospital'
+    user.memberships.make collection: collection
+    collection
   end
 
   def create_site_for(collection)
-  	site = Site.make(:collection => collection)
+    collection.sites.make(:name => 'Health Center', :lng => 14.3574, :lat => 26.7574)
   end
 
   def create_layer_for
@@ -15,6 +15,7 @@ module Capybara::CollectionHelper
   end
 
   def create_field_for
-  	field = Field::TextField.make(:name => 'Central Hospital Layer 1 Field', collection_id: 1, layer_id: 1)
+  	Field::TextField.make(:name => 'Central Hospital Layer 1 Field', collection_id: 1, layer_id: 1)
   end
+
 end
