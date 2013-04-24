8af1b1dc800d9c8b0aff5146c89519e0b6ffe0a4
German Batiston <gbatiston@manas.com.ar>
2013-04-24 11:27:58 -0300

    Change_password test added

---
+++ spec/integration/account/change_password_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper' 
+
+describe "change_password" do 
+ 
+it " should change password", js:true do
+    @user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
+    @collection = create_collection_for(@user) 
+    login_as (@user)
+    visit collections_path
+    sleep 2
+    find(:xpath, '//div[@id="toolbar"]/ul[2]/li[2]/a').click
+    within "form#edit_user" do 
+      fill_in "user_current_password", :with => @user.password
+      fill_in "user_password", :with => 'dexmor.15'
+      fill_in "user_password_confirmation", :with => 'dexmor.15'
+    end
+    click_button 'Update'
+    sleep 1
+    page.save_screenshot 'Change_password.png'
+    page.should have_content 'Account updated successfully'
+  end
+end
