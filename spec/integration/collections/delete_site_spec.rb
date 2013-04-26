748b17dc4670274e1df4509bd9fffcfca3a19e79
German Batiston <gbatiston@manas.com.ar>
2013-04-26 16:01:21 -0300

    delete site test added

---
+++ spec/integration/collections/delete_site_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper' 
+
+describe "delete_site" do 
+ 
+  it "should delete site", js:true do
+    user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
+    page.save_screenshot 'create_collection.png'
+    collection = create_collection_for (user)
+    create_site_for (collection)
+    login_as (user)
+    visit collections_path
+    find(:xpath, '//div[@id="collections-main"]/div[1]/div[2]/table/tbody/tr[1]/td/button').click
+    find(:xpath, '//div[@id="collections-main"]/div[1]/div[2]/table/tbody/tr[1]/td/button').click
+    click_link 'Delete Site'
+    page.driver.browser.switch_to.alert.accept 
+    sleep 2
+    page.save_screenshot "Delete_site.png"
+    page.should have_no_content ("Health Center")
+  end
+end
+
