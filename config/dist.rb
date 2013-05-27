1429581c2cf6a80b769db7d68fdd63f9c87ce055
Carolina Hadad <chadad@manas.com.ar>
2013-05-27 16:08:34 -0300

    Updated dist gem from repo + aplication renamed in dist config file

--- config/dist.rb
+++ config/dist.rb
@@ -1,4 +1,4 @@
-set :application, 'resourcemap'
+set :application, 'resource_map'
 set :version, '2.0.1'
 set :maintainer, 'Carolina Hadad <chadad@manas.com.ar>'
 set :description, 'InSTEDD Resource Map'
