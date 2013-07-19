9099f1f709057ad5d57ab29d4f275a5f95d9dde3
Carolina Hadad <chadad@manas.com.ar>
2013-07-19 16:08:05 -0300

    Trying to fix weird repo status in stg server.. adding tabs

--- db/migrate/20130705165537_serialize_existing_fields_config.rb
+++ db/migrate/20130705165537_serialize_existing_fields_config.rb
@@ -2,17 +2,18 @@ class SerializeExistingFieldsConfig < ActiveRecord::Migration
   def up
     connection.select_rows("SELECT id, config FROM fields").each do |id, config|
 
-      next if config.blank?
-      begin
-        config = YAML.load(config)
-      rescue Exception => ex
+        next if config.blank?
         begin
-          config.force_encoding "iso8859-1"
           config = YAML.load(config)
-        rescue Exception => ex2
-          next
+        rescue Exception => ex
+          begin
+           config.force_encoding "iso8859-1"
+          config = YAML.load(config)
+          rescue Exception => ex2
+            next
+          end
         end
-      end
+
       binary_config = MarshalZipSerializable.dump(config)
       if binary_config.nil?
         connection.execute("UPDATE fields SET config=NULL WHERE id=#{id}")
