da58748fb4821d033381dbb0efa8dc034b9e0408
Carolina Hadad <chadad@manas.com.ar>
2013-10-18 16:49:33 -0300

    Schema version updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131003192147) do
+ActiveRecord::Schema.define(:version => 20131018162412) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
