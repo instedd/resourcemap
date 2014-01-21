a559b534c234d63da96d51affde657d8ab378231
vtiffenberg <vtiffenberg@manas.com.ar>
2014-01-21 14:52:21 -0300

    Removed extra puts and added clarifications

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -69,8 +69,8 @@ class CollectionsController < ApplicationController
   end
 
   def create
-    collection.name = "My collection" if request.xhr? && collection.name.blank?
-    p collection
+    # In case user uploads logo into new collection, name is necessary for save.
+    collection.name = "My collection" if request.xhr? && collection.name.blank? && !collection.logo.blank?
     if current_user.create_collection collection
       current_user.collection_count += 1
       current_user.update_successful_outcome_status
@@ -86,6 +86,7 @@ class CollectionsController < ApplicationController
 
   def update
     if request.xhr? && !params[:collection][:logo].blank? && collection.update_attributes(params[:collection])
+      # Logo upload via ajax
       render_json({url: collection.logo_url(:grayscale)})
     elsif collection.update_attributes params[:collection]
       collection.recreate_index
