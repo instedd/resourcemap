759bfe9ed2b29317a7a37a41a76ca476605f548b
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2014-05-09 13:19:14 -0300

    Adding *.po's paths
    --HG--
    branch : i18n

--- config/application.rb
+++ config/application.rb
@@ -80,6 +80,7 @@ module ResourceMap
 end
 
 Haml::MagicTranslations.enable(:i18n)
+I18n.load_path += Dir["locale/en/*.{po}"]
 FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
 FastGettext.default_text_domain = 'app'
 FastGettext.default_available_locales = ['en']
