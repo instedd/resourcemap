b2f2bf39288dfec02056a33236a85b7f035e9b39
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-06-12 15:21:43 -0300

    Make FieldHistory more resilient: if the original field was destroyed use a fake in-memory one in helper methods

---
+++ spec/models/field_history_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe FieldHistory do
+  it "reindexes collection after destroy field" do
+    Timecop.freeze(Time.now) do
+      collection = Collection.make
+      layer = collection.layers.make
+      numeric = layer.numeric_fields.make code: 'numeric'
+      site = collection.sites.make properties: {numeric.es_code => 1}
+
+      snapshot = collection.snapshots.make date: Time.now
+      numeric.destroy
+
+      snapshot.collection.field_histories.each do |field_history|
+        field_history.allow_decimals?
+      end
+    end
+  end
+end
