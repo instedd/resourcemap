6c3b552da6d9e7e1b716785941f8bed0c426a858
Mauri <mmuller@manas.com.ar>
2014-11-05 18:01:07 -0300

    Account tests refactoring

--- spec/integration/account/account_spec.rb
+++ spec/integration/account/account_spec.rb
@@ -1,11 +1,86 @@
 require 'spec_helper'
 
-describe "Accounts", :type => :request do
+describe "account", :type => :request do
+  let (:user) do
+    new_user
+  end
+
+  before :each do
+    visit collections_path
+  end
+
+  it "should login", js:true do
+
+    visit collections_path
+    click_link 'Log in'
+
+    within login_form do
+      fill_in  "Email", :with => user.email
+      fill_in  "Password", :with => "1234567"
+      click_button('Log In')
+    end
+
+    expect(page).to have_content('Signed in successfully.')
+
+  end
+
+  it " should not change password", js:true do
+
+    login_as (user)
+    visit collections_path
+    find_by_id('User').click
+    click_link 'Settings'
+
+    within "form#edit_user" do
+      fill_in "user_current_password", :with => user.password
+      fill_in "user_password", :with => 'invalid'
+      click_button 'Update'
+    end
+
+    expect(page).to have_content "Password confirmation doesn't match Password"
+
+  end
+
+  it " should log out", js:true do
+
+    login_as (user)
+    visit collections_path
+    find_by_id('User').click
+    click_link 'Sign out'
+
+    expect(page).to have_content('Signed out successfully.')
+
+  end
 
   it "should create an account", js:true do
-   user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
-   login_as (user)
-   visit collections_path
+
+    click_link 'Create account'
+    page.has_content? "form#new_user"
+
+    within login_form do
+      fill_in  "Email", :with => 'user@manas.com.ar'
+      fill_in  "Password", :with => "1234567"
+      fill_in  "Password confirmation", :with => "1234567"
+      fill_in  "Phone number", :with => "1234567"
+      click_button ('Create account')
+    end
+
+  end
+
+  it "should fail to login", js:true do
+
+    click_link 'Log in'
+
+    within login_form do
+      fill_in  "Email", :with => user.email
+      fill_in  "Password", :with => "Password01"
+      click_button('Log In')
+    end
+
+    page.save_screenshot 'login_fail.png'
+    expect(page).to have_content("Invalid email or password.")
+
   end
 
 end
+
--- spec/support/capybara/account_helper.rb
+++ spec/support/capybara/account_helper.rb
@@ -1,5 +1,11 @@
 module Capybara::AccountHelper
+
   def login_form
     "form#new_user"
   end
+
+  def new_user
+    user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
+  end
+
 end
