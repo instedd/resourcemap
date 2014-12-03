053a204eae07a56ea740334ef3caf9fc4604f620
Franco Bulgarelli <fbulgarelli@manas.com.ar>
2014-12-03 12:16:25 -0300

    Adding test for case and accents insensitive search

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -337,6 +337,19 @@ describe Search, :type => :model do
       assert_results collection.new_search.full_text_search("Censús"), a_site
     end
 
+    it "searches case-insensitive" do
+      a_site = collection.sites.make :name => "cutralco"
+      assert_results collection.new_search.full_text_search("CutralCo"), a_site
+    end
+
+    describe "searches accents-insensitive" do
+      let!(:colon) { collection.sites.make  name: 'colón' }
+      let!(:paternal) { collection.sites.make name: 'paternal' }
+
+      it { assert_results collection.new_search.full_text_search("colon"), colon }
+      it { assert_results collection.new_search.full_text_search("páternal"), paternal }
+    end
+
     it "escapes symbols" do
       assert_results collection.new_search.full_text_search("hello?/{#."), site4
     end
