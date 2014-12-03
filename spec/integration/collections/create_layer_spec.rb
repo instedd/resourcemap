0a139ad9700880a23774b9fc183c580b0a30b4dd
Mauri <mmuller@manas.com.ar>
2014-12-03 14:31:12 -0300

    Fixed 'Create Layer' test

--- spec/integration/collections/create_layer_spec.rb
+++ spec/integration/collections/create_layer_spec.rb
@@ -1,16 +1,15 @@
-require 'spec_helper' 
+require 'spec_helper'
 
-describe "create_layer", :type => :request do 
- 
+describe "create_layer", :type => :request do
+
   it "should create layer", js:true do
     @user = User.make(:email => 'user@manas.com.ar', :password => '1234567', :phone_number => '855123456789')
     page.save_screenshot 'edit_layer.png'
     collection = create_collection_for(@user)
     login_as (@user)
     visit collections_path
-    sleep 2
-    page.find(:xpath, '//div[@id="collections-main"]/div[1]/div[2]/table/tbody/tr[1]/td/button').click
-    page.find(:xpath, '//div[@id="collections-main"]/div[1]/div[1]/button[2]').click
+    find(:xpath, first_collection_path).click
+    find("#collections-main").find("button.fconfiguration").click
     click_link "Layers"
     click_button 'Add new layer'
     fill_in 'name', :with => 'Test'
@@ -20,7 +19,7 @@ describe "create_layer", :type => :request do
    	click_button 'Save layer'
    	expect(page).to have_content "Saving layer, please wait..."
    	sleep 2
-   	expect(page).to have_content "Layer 'Test' successfully created"
-   	page.save_screenshot "Create layer.png"
+   	expect(page).to have_content "Layer 'Test' successfully saved"
+
   end
 end
--- spec/integration/collections/layers/layers_spec.rb
+++ spec/integration/collections/layers/layers_spec.rb
@@ -26,4 +26,24 @@ describe "layer", :type => :request, uses_collections_structure: true do
 
   end
 
+  it "should create layer", js:true do
+
+    who_african_region.memberships.create! :user_id => user.id, :admin => true
+    login_as (user)
+    visit collections_path
+    find(:xpath, first_collection_path).click
+    find("#collections-main").find("button.fconfiguration").click
+    click_link "Layers"
+    click_button 'Add new layer'
+    fill_in 'name', :with => 'Test'
+    click_button 'Add new text field'
+    fill_in 'field_name', :with => 'Test name'
+    fill_in 'code', :with => 'Code'
+    click_button 'Save layer'
+
+    expect(page).to have_content "Saving layer, please wait..."
+    expect(page).to have_content "Layer 'Test' successfully saved"
+
+  end
+
 end
