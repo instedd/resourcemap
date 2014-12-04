fd3efa1d348c90c43fd91c240c7b0d426073ab1b
Franco Bulgarelli <fbulgarelli@manas.com.ar>
2014-12-04 17:36:29 -0300

    Fixing some tests

--- spec/models/collection_elasticsearch_concern_spec.rb
+++ spec/models/collection_elasticsearch_concern_spec.rb
@@ -15,27 +15,37 @@ describe Collection::ElasticsearchConcern, :type => :model do
     expect(client.indices.exists(index: collection.index_name)).to be_falsey
   end
 
-  it "create proper index name" do
-    index_name = Collection.index_name 32
-    expect(index_name).to eq("collection_test_32")
+  describe "create proper index name" do
+    it do
+      index_name = Collection.index_name 32
+      expect(index_name).to eq("active_collection_test_32")
+    end
 
-    index_name_for_snapshot = Collection.index_name 32, snapshot_id: 12
-    expect(index_name_for_snapshot).to eq("collection_test_32_12")
+    it do
+      index_name_for_snapshot = Collection.index_name 32, snapshot_id: 12
+      expect(index_name_for_snapshot).to eq("active_collection_test_32_12")
+    end
 
-    collection = Collection.make
-    index_name_for_user_without_collection = Collection.index_name collection.id, user: User.make
-    expect(index_name_for_user_without_collection).to eq("collection_test_#{collection.id}")
+    it do
+      collection = Collection.make
+      index_name_for_user_without_collection = Collection.index_name collection.id, user: User.make
+      expect(index_name_for_user_without_collection).to eq("active_collection_test_#{collection.id}")
+    end
 
-    collection = Collection.make
-    index_name_for_user_without_snapshot = Collection.index_name(collection.id, user: User.make)
-    expect(index_name_for_user_without_snapshot).to eq("collection_test_#{collection.id}")
+    it do
+      collection = Collection.make
+      index_name_for_user_without_snapshot = Collection.index_name(collection.id, user: User.make)
+      expect(index_name_for_user_without_snapshot).to eq("active_collection_test_#{collection.id}")
+    end
 
-    user = User.make
-    collection = Collection.make
-    collection.snapshots.create! date: Time.now, name: 'last_year'
-    snapshot = collection.snapshots.first
-    UserSnapshot.make :user => user, :snapshot => snapshot
-    index_name_for_user_with_snapshot = Collection.index_name(collection.id, user: user)
-    expect(index_name_for_user_with_snapshot).to eq("collection_test_#{collection.id}_#{snapshot.id}")
+    it do
+      user = User.make
+      collection = Collection.make
+      collection.snapshots.create! date: Time.now, name: 'last_year'
+      snapshot = collection.snapshots.first
+      UserSnapshot.make :user => user, :snapshot => snapshot
+      index_name_for_user_with_snapshot = Collection.index_name(collection.id, user: user)
+      expect(index_name_for_user_with_snapshot).to eq("active_collection_test_#{collection.id}_#{snapshot.id}")
+    end
   end
 end
