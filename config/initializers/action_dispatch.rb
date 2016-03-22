59d86027feb37d767a32ced29cdecbf110ae9518
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-03-22 18:09:41 -0300

    Customize JSON export for UploadedFile
    Since we have Poirot enabled for Resourcemap, attempting to serialize
    the request parameters to send for logging resulted in errors when
    handling Http::UploadedFile. Fix this by customizing the encoded fields.
    
    Partially fixes #807.

---
+++ config/initializers/action_dispatch.rb
@@ -0,0 +1,11 @@
+class ActionDispatch::Http::UploadedFile
+  # When serializing an UploadedFile to JSON, don't try to serialize the file
+  # contents since this breaks the JSON encoders. This is especially important
+  # for logging with Poirot.
+  def as_json(options = nil)
+    %w(content_type headers original_filename size).inject({}) do |hash, attr|
+      hash[attr] = send(attr)
+      hash
+    end
+  end
+end
