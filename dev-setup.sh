3dba31d7516312a74359fb7130faebc651178729
Brian J. Cardiff <bcardiff@gmail.com>
2018-02-22 01:28:33 -0300

    Add docker dev setup script

---
+++ dev-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker-compose pull
+docker-compose up -d db elasticsearch redis
+docker-compose run --rm --no-deps web bundle
+docker-compose run --rm --no-deps web rake db:setup
+docker-compose run --rm --no-deps web rake db:test:prepare
