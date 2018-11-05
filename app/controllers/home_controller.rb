72b0c3c4228cdaf31ef66bc7f0e341122ea3a426
pmallol <pmallol@manas.com.ar>
2018-11-05 14:18:11 -0300

    Destroys Intercom session on logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  after_action :intercom_shutdown, only: [:index]
+
   def index
     redirect_to collections_path if !current_user.is_guest && !params[:explicit]
+  end
+
+  protected
+  def intercom_shutdown
+    IntercomRails::ShutdownHelper.intercom_shutdown(session, cookies, request.domain)
   end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < Devise::SessionsController
+  after_action :prepare_intercom_shutdown, only: [:destroy]
+
+  protected
+  def prepare_intercom_shutdown
+    IntercomRails::ShutdownHelper.prepare_intercom_shutdown(session)
+  end
+end
