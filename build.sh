697b37059416cd243a0bf0aeb622ff15a1e31a68
Matias Garcia Isaia <mgarcia@manas.tech>
2021-11-10 20:09:08 -0300

    Migrate CI to Github Actions
    So long, Travis - and thanks for all the fish!

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eo pipefail
+
+source <(curl -s https://raw.githubusercontent.com/manastech/ci-docker-builder/a2d44f8b7f155afe5a9652252cfb262871846a41/build.sh)
+
+dockerSetup
+echo $VERSION > VERSION
+dockerBuildAndPush
